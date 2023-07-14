e84241a367f361eaea95dad6bec621db88ac7e7f
brk-a <fnjakai@outlook.com>
2023-07-14 04:02:44 +0000

    bestSum: part 2, memoised

--- 0x01-dynamic_programming/bestSum.js
+++ 0x01-dynamic_programming/bestSum.js
@@ -134,3 +134,81 @@ const arr = [
 arr.forEach(obj => {
     console.log(`input: (${obj.targetSum}, [${obj.numbers}]): return: ${bestSum(obj.targetSum, obj.numbers)}`);
 })
+
+/**
+ * /**bestSum has exponential time complexity and quadratic space complexity
+ * 
+ * let m be the target sum and n be the length of the array of candidates
+ * 
+ * worst-case scenario max-depth: the array contains `1` (algo will subtract 1 at each edge until base case)
+ * therefore, the worst-case max depth of the tree is m
+ * 
+ * the operation `return [...remResult, i]` is, technically, an iteration process. it takes a linear #steps
+ * to copy each element of the array then add a new element. the max-level length of the 
+ * iteration is the length of the array; that is, m. say targetSum = 50 and numbers = [1, 1, 1, 1 ].
+ * the #steps to iterate on remResult would be 50 which, non-coincidentally, happens to be m. 
+ * 
+ * worst-case scenario branching factor: at level 1, the algo must branch n times; one for each member of the
+ * array. may or may not happen at other levels, therefore, worst-case scenario branching factor is n
+ * 
+ * base of the exponent is the branching factor; power is the depth of the tree, therefore (n^m)
+ * 
+ * every recursive call will have its own `shortestCombination` variable; worst-case scenario, length will be m 
+ * m variables with values of length m, at worst, equals (m*m) or (m^2) max space required 
+ * 
+ * time complexity is O((n^m) * m) and space complexity is O(m^2)
+ * 
+*/
+
+const bestSumMemo = (targetSum, numbers, memo={}) => {
+    if(targetSum in memo) return memo[targetSum]
+    if(targetSum===0) return []
+    if(targetSum<0) return null
+
+    let shortestCombination = null
+
+    for(let i of numbers){
+        const remainder = targetSum - i
+        const remainderCombination = bestSum(remainder, numbers, memo)
+        
+        if(remainderCombination!==null){
+            const combination = [...remainderCombination, i]
+            if(shortestCombination===null || combination.length<shortestCombination.length){
+                shortestCombination = combination
+            }
+        }
+    }
+
+    memo[targetSum] = shortestCombination
+    return shortestCombination
+}
+
+arr.forEach(obj => {
+    console.log(`input: (${obj.targetSum}, [${obj.numbers}]): return: ${bestSumMemo(obj.targetSum, obj.numbers)}`);
+})
+
+/**
+ * bestSumMemo has quadratic time and space complexity
+ * 
+ * #recursive calls are the same as bestSum -> n*m.
+ * each recursive call requires a copy operation (spread the array and add a new member).
+ * array will be, at most, m elements long.
+ * (n*m) recursive calls each making m copy ops equals (n*m*m) or n*(m^2)
+ * 
+ * keys of memo obj are all the unique values of `targetSum`; value can be  null or an array.
+ * max length of an array is m.
+ * m keys with values of length m, at worst, equals (m*m) or (m^2) max space required 
+ * 
+ * time complexity is O(n*(m^2)) and space complexity is O(m^2)
+ * 
+ * recall, for bestSum: time complexity is O((n^m) * m) and space complexity is O(m^2)
+ * trade-offs
+ *      1. time: decreased complexity from exponential to quadratic
+ *      2. space: increased complexity from linear to quadratic 
+ *      3. overall: the algo is quicker but requires more memory
+ * 
+ * insights:
+ *      1. prefer the memoised algo to the brute force, overall
+ *      2. pick the brute force algo when you are not in a hurry and have limited memory
+ *      3. pick the memoised algo when you are in a hurry and have memory to spare
+ */
