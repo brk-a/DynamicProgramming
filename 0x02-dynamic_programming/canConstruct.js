cdd556ae9c5f9a26d225c4efd74b9606c6697783
brk-a <fnjakai@outlook.com>
2023-07-28 04:09:56 +0300

    canConstruct: solved using tabulation

--- 0x02-dynamic_programming/canConstruct.js
+++ 0x02-dynamic_programming/canConstruct.js
@@ -28,7 +28,7 @@
  */
 
 /**
- * take canConstruct('', ['oh', ',', 'fox', 'sake', '!'])
+ * take canConstruct('', ['oh,', 'for', 'fox', 'sake', '!'])
  * 
  * target is an empty string; no way to construct an empty string
  * 
@@ -58,69 +58,86 @@
  *      3. table[i] === true: for j in `wordBank` do:
  *          a. evaluate k = wordBank[j].length
  *          b. add i and k
- *          c. check if i+k > targetSum
- *          d. i+k > targetSum: continue
- *          e. i+k <= targetSum: check if wordbank[j].slice[0, 1]===target[i+1]
- *          f. wordbank[j].slice[0, 1]!==target[i+1]: continue
- *          g. wordbank[j].slice[0, 1]===target[i+1]: set table[i+k] to `true`
+ *          c. check if i+k > target.length
+ *          d. i+k > target.length: continue
+ *          e. i+k <= target.length: check if wordbank[j]===target.slice(i, i+k)
+ *          f. wordbank[j]!==target.slice(i, i+k]): continue
+ *          g. wordbank[j]===target.slice(i, i+k]): set table[i+k] to `true`
  *      4. return table[target.length]
  * 
- * first pass. current position: table[0] = `true`,  wordBank = ['ab', 'abc', 'cd', 'def', 'abcd']
- *      1. table[0] = `true`; traverse `wordBank`
+ * first pass. current position: table[0] = `true`  wordBank = ['ab', 'abc', 'cd', 'def', 'abcd']
+ *      1. table[0] = target.slice(0, 0) = '' = `true`; traverse `wordBank`
  *      2. traversing `wordBank`
- *          a. 0+2=2 <= 7; wordBank[0].slice[0,1]===target[2]: set table[2] to `true`
- *          b. 0+3=3 <= 7; wordBank[0].slice[0,1]===target[2]: set table[2] to `true`
- *          c. 0+2=2 <= 7; wordBank[0].slice[0,1]===target[2]: set table[2] to `true`
- *          d. 0+3=3 <= 7; wordBank[0].slice[0,1]===target[2]: set table[2] to `true`
- *          e. 0+4=4 <= 7; 
+ *          a. 0+2=2 <= 7; wordBank[0]===target.slice(0, 2): set table[2] to `true`
+ *          b. 0+3=3 <= 7; wordBank[1]===target.slice(0, 3): set table[3] to `true`
+ *          c. 0+2=2 <= 7; wordBank[2]!==target.slice(0, 2): continue
+ *          d. 0+3=3 <= 7; wordBank[3]!==target.slice(0, 3): continue
+ *          e. 0+4=4 <= 7; wordBank[4]===target.slice(0, 4): set table[4] to `true`
  *      table viz:
- *          [`true`, `false`, `false`, `true`, `true`, `false`, `false`, `true`]
+ *          [`true`, `false`, `true`, `true`, `true`, `false`, `false`]
  * 
- * second pass. current position: table[1] = `false`,  numbers = [5, 3, 4, 7]
- *      1. table[1] = `false`;  do not traverse `numbers`
+ * second pass. current position: table[1] = `false`,  wordBank = ['ab', 'abc', 'cd', 'def', 'abcd']
+ *      1. table[1] = target.slice(0, 1) = 'a' = `false`;  do not traverse `wordBank`
  *      table viz:
- *          [`true`, `false`, `false`, `true`, `true`, `false`, `false`, `true`]
+ *          [`true`, `false`, `true`, `true`, `true`, `false`, `false`]
  * 
- * third pass. current position: table[2] = `true`,  numbers = [5, 3, 4, 7]
- *      1. table[2] = `true`; traverse `numbers`
- *      2. traversing `numbers`
- *          a. 2+5=7 <= 7; set table[7] to `true`
- *          b. 2+3=5 <= 7; set table[5] to `true`
- *          c. 2+4=6 <= 7; set table[6] to `true`
- *          d. 2+7=9 <= 7; continue
+ * third pass. current position: table[2] = `true`,  wordBank = ['ab', 'abc', 'cd', 'def', 'abcd']
+ *      1. table[2] = target.slice(0, 2) = 'ab' = `true`; traverse `wordBank`
+ *      2. traversing `wordBank`
+ *          a. 2+2=4 <= 7; wordBank[0]!==target.slice(2, 4): continue
+ *          b. 2+3=5 <= 7; wordBank[1]!==target.slice(2, 5): continue
+ *          c. 2+2=4 <= 7; wordBank[2]===target.slice(2, 4): set table[4] to `true`
+ *          d. 2+3=5 <= 7; wordBank[3]!==target.slice(2, 5): continue
+ *          e. 2+4=6 <= 7; wordBank[4]!==target.slice(2, 6): continue
  *      table viz:
- *          [`true`, `false`, `false`, `true`, `true`, `true`, `true`, `true`]
+ *           [`true`, `false`, `true`, `true`, `true`, `false`, `false`]
  * 
- * fourth pass. current position: table[3] = `true`,  numbers = [5, 3, 4, 7]
- *      1. table[4] = `true`; traverse `numbers`
- *      2. traversing `numbers`
- *          a. 3+5=8 > 7; continue
- *          b. 3+3=6 <= 7; set table[6] to `true`
- *          c. 3+4=7 <= 7; set table[7] to `true`
- *          d. 3+7=10 > 7; continue
+ * fourth pass. current position: table[3] = `true`,  wordBank = ['ab', 'abc', 'cd', 'def', 'abcd']
+ *      1. table[3] = target.slice(0, 3) = 'abc' = `true`; traverse `wordBank`
+ *      2. traversing `wordBank`
+ *          a. 3+2=5 <= 7; wordBank[0]!==target.slice(3, 5): continue
+ *          b. 3+3=6 <= 7; wordBank[1]!==target.slice(3, 6): continue
+ *          c. 3+2=5 <= 7; wordBank[2]!==target.slice(3, 5): continue
+ *          d. 3+3=6 <= 7; wordBank[3]===target.slice(3, 6): set table[6] to `true`
+ *          e. 3+4=7 <= 7; wordBank[4]!==target.slice(3, 7): continue
  *      table viz:
- *          [`true`, `false`, `false`, `true`, `true`, `true`, `true`, `true`]
+ *           [`true`, `false`, `true`, `true`, `true`, `false`, `true`]
  * 
- * fifth pass. current position: table[4] = `true`,  numbers = [5, 3, 4, 7]
- *      1. table[4] = `true`; traverse `numbers`
- *      2. traversing `numbers`
- *          a. 4+5=9 > 7;  continue
- *          b. 4+3=7 <= 7; set table[7] to `true`
- *          c. 4+4=8 > 7;  continue
- *          d. 4+7=11 > 7; continue
+ * fifth pass. current position: table[4] = `true`,  wordBank = ['ab', 'abc', 'cd', 'def', 'abcd']
+ *      1. table[4] = target.slice(0, 4) = 'abcd' `true`; traverse `wordBank`
+ *      2. traversing `wordBank`
+ *          a. 4+2=6 <= 7; wordBank[0]!==target.slice(4, 6): continue
+ *          b. 4+3=7 <= 7; wordBank[1]!==target.slice(4, 7): continue
+ *          c. 4+2=6 <= 7; wordBank[2]!==target.slice(4, 6): continue
+ *          d. 4+3=7 <= 7; wordBank[3]!==target.slice(4, 7): continue
+ *          e. 4+4=8 > 7;  continue
  *      table viz:
- *          [`true`, `false`, `false`, `true`, `true`, `true`, `true`, `true`]
+ *          [`true`, `false`, `true`, `true`, `true`, `false`, `true`]
  * 
- * sixth pass. current position: table[5] = `true`,  numbers = [5, 3, 4, 7]
- *      1. table[5] = `true`; traverse `numbers`
- *      2. traversing `numbers`
- *          a. 5+5=10 > 7; continue
- *          b. 5+3=8 > 7; continue
- *          c. 5+4=9 > 7; continue
- *          d. 5+7=12 > 7; continue
+ * sixth pass. current position: table[5] = `false`,  wordBank = ['ab', 'abc', 'cd', 'def', 'abcd']
+ *      1. table[5] = target.slice(0, 5) = 'abcde' = `false`;  do not traverse `wordBank`
  *      table viz:
- *          [`true`, `false`, `false`, `true`, `true`, `true`, `true`, `true`]
+ *          [`true`, `false`, `true`, `true`, `true`, `false`, `true`]
  * 
+ * seventh pass. current position: table[6] = `true`,  wordBank = ['ab', 'abc', 'cd', 'def', 'abcd']
+ *      1. table[6] = target.slice(0, 6) = 'abcdef' `true`; traverse `wordBank`
+ *      2. traversing `wordBank`
+ *          a. 6+2=8 > 7; continue
+ *          b. 6+3=9 > 7; continue
+ *          c. 6+2=8 > 7; continue
+ *          d. 6+3=9 > 7; continue
+ *          e. 6+4=10 > 7; continue
+ *      table viz:
+ *          [`true`, `false`, `true`, `true`, `true`, `false`, `true`]
+ * 
+ * return
+ *      table[target.length] = table[6] = `true`
+ *      meaning that it is possible to create the string `abcdef` by concatenating elements of
+ *      `wordBank`
+ *      sanity check: abcdef = abc + def
+ * 
+ * possible early return:
+ *      notice that table[target.length] = table[6] = `true` at the fourth pass
  */
 
 /**
@@ -148,3 +165,54 @@
  * 
  * time complexity is O((n^m) * m) and space complexity is O(m^2)
  */
+
+const canConstruct = (target, wordBank) => {
+    const table = new Array(target.length+1).fill(false)
+    table[0] = true
+
+    for(let i=0; i<=target.length; ++i){
+        if(table[i]===true){
+            for(let j of wordBank){
+                const k = j.length
+                if(target.slice(i, i+k)===j){
+                    table[i+k] = true
+                }
+            }
+
+            // if(table[target]===true) return table[target] //optional early return
+        }
+    }
+    
+    return table[target.length]
+}
+
+arr = [
+    {
+        target: 'abcdef',
+        wordBank: ['ab', 'abc', 'cd', 'def', 'abcd'],
+    },
+    {
+        target: 'skateboard',
+        wordBank: ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'],
+    },
+    {
+        target: 'enterapotentpot',
+        wordBank: ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'],
+    },
+    {
+        target: 'bougainvellia',
+        wordBank: ['ou', 'gain', 'vil', 'bo', 'liea'],
+    },
+    {
+        target: '',
+        wordBank: ['oh,', 'for', 'fox', 'sake', '!'],
+    },
+    {
+        target: 'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef',
+        wordBank: ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'],
+    },
+]
+
+arr.forEach(obj => {
+    console.log(`input: ${obj.target}: return: ${canConstruct(obj.target, obj.wordBank)}`);
+})
