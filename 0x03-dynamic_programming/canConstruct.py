b85eb7a7dc8f6ae6dc824d1712809245920ab4f1
brk-a <fnjakai@outlook.com>
2023-08-16 04:26:07 +0300

    canConstruct

---
+++ 0x03-dynamic_programming/canConstruct.py
@@ -0,0 +1,6 @@
+'''
+write a function `canConstruct()` that takes in a targetString and
+an array of strings.
+said fn should return a boolean indicating whehter or not `targetString`
+van be constructed from the array of strs
+'''
